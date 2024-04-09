5c076a01dc8741a8fefbc0fd1e408373616edca6
AlwaysAadiii <165683636+AlwaysAadiii@users.noreply.github.com>
2024-04-09 09:43:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Reelogram'
 
+# Health check endpoint
+@app.route('/health')
+def health_check():
+    return 'OK'
 
 if __name__ == "__main__":
     app.run()
